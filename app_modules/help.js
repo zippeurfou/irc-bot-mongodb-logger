b5ddc44e1529445b6a437f9d3a4f2b090c3348eb
zippeurfou <zippeurfou@gmail.com>
2015-05-31 12:03:35 -0400

    editing message

--- app_modules/help.js
+++ app_modules/help.js
@@ -11,7 +11,7 @@ exports.initialize = function(bot) {
 function logEvents(bot) {
     bot.on("new user", function(user){
         console.log("hey you",user);
-        var answer="Welcome "+user.nick+" on WormNet! If you have any problem please read this tutorial:  http://www.tus-wa.com/forums/dos-help/newbie-guide-book-26874/";
+        var answer="Welcome to WormNet! If you have any problem, read this tutorial: http://www.tus-wa.com/forums/dos-help/newbie-guide-book-26874/ Here`s info about available game modes: http://worms2d.info/Schemes With WormNet snooper you can chat and enter games easier: http://worms2d.info/Great_Snooper If you want to play league games, visit tus-wa.com";
         bot.pm(user.nick,answer);
     });
     
